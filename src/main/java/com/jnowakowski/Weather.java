5f0c452baf8b6665f0306005752d0b0c711b43b5
Jakub Nowakowski <jnowakow@cisco.com>
2017-07-03 14:25:38 +0200

    Sample REST service

---
+++ src/main/java/com/jnowakowski/Weather.java
@@ -0,0 +1,16 @@
+package com.jnowakowski;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("weather")
+@Produces(MediaType.TEXT_PLAIN)
+public class Weather {
+
+    @GET
+    public String get() {
+        return "get weather fake response";
+    }
+}
---
+++ src/test/java/WeatherTest.java
@@ -0,0 +1,25 @@
+import com.jnowakowski.Weather;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTestNg;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+public class WeatherTest extends JerseyTestNg.ContainerPerClassTest {
+
+    private static final String RESOURCE = "weather";
+
+    @Test
+    public void test() {
+        Response response = target(RESOURCE).request().get();
+        Assert.assertEquals(response.getStatus(), 200);
+        Assert.assertEquals(response.readEntity(String.class), "get weather fake response");
+    }
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(Weather.class);
+    }
+}
