068604eac453406a31b79fab50f7632a4509a443
Jakub Nowakowski <jakub.nowakowski@amartus.com>
2017-07-27 17:32:18 +0200

    Node JS updates

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,56 +1,24 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var port = process.env.PORT || 3000;
+var express     =   require('express');
+var app         =   express();
+var bodyParser  =   require("body-parser");
+var router      =   express.Router();
+var port        =   3001;
 
-var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient
-var url = "mongodb://192.168.9.22:27017/rocketDB";
-var tableName = "recipes"
+//  https://codeforgeek.com/2015/08/restful-api-node-mongodb/
 
-// Create DB
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
+var mongoose    =   require("mongoose");
+mongoose.connect('mongodb://192.168.9.22:27017/rocketDB');
 
-    db.createCollection(table, function(err, res) {
-        if (err) throw err;
-        console.log("Table created: " + tableName);
-        db.close();
-  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended" : false}));
+
+router.get("/",function(req,res){
+    res.json({"error" : false,"message" : "Hello World"});
 });
 
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-  if(!err) { console.log("We are connected"); }
 
-});
-
-app.get('/listUsers', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       console.log( data );
-       res.json(JSON.parse(data));
-   });
-})
-
-var user = {
-   "user4" : {
-      "name" : "mohit",
-      "password" : "password4",
-      "profession" : "teacher",
-      "id": 4
-   }
-}
-
-app.post('/addUser', function (req, res) {
-   // First read existing users.
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-       data = JSON.parse( data );
-       data["user4"] = user["user4"];
-       console.log( data );
-       res.json(data);
-   });
-})
+app.use('/',router);
 
 var server = app.listen(port)
 
-console.log('todo list RESTful API server started on: ' + port);
+console.log('RESTful API server started on: ' + port);
