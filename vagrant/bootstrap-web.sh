c07e8136bdb6e9f8fbeceae7d4e89eb0f1815aac
Jakub Nowakowski <jakub.nowakowski@amartus.com>
2017-07-27 15:44:35 +0200

    ansible for java 8 install

--- vagrant/bootstrap-web.sh
+++ vagrant/bootstrap-web.sh
@@ -7,5 +7,15 @@
 #   ln -fs /vagrant /var/www
 # fi
 
+echo "Installing Java 8"
+sudo apt-get install -y software-properties-common python-software-properties
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+echo "Setting environment variables for Java 8"
+sudo apt-get install -y oracle-java8-set-default
+
+echo "Installing Tomcat 8"
 sudo apt-get update
 sudo apt-get -y install tomcat8
